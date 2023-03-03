dbf41d8f012deee3176ee3696b989c6b819fc526
Colin Tracey <crtracey@gmail.com>
2023-03-03 13:10:42 -0800

    Fix 4 player map
    Some cities are different on 4p map than 3p map.
    Make adjustements accordingly.

--- src/game/maps.ts
+++ src/game/maps.ts
@@ -268,6 +268,16 @@ export const addNeighbors = (map: GameMap): GameMap => {
   return map;
 };
 
+export const adjustCities4p = (map: GameMap): GameMap => {
+  map.cities.Kampen.offices = [{ color: 1, merch: true }, { color: 3 }];
+  map.cities.Bremen.offices = [{ color: 0, merch: true }, { color: 2 }];
+  map.cities.Luneburg.offices = [{ color: 1, merch: true }, { color: 3 }];
+  map.cities.Goslar.offices = [{ color: 0 }, { color: 3 }];
+  map.cities.Dortmund.offices = [{ color: 0, merch: true }, { color: 1 }, { color: 2 }];
+  map.cities.Gottingen.offices = [{ color: 0 }, { color: 0, merch: true }, { color: 2 }];
+  return map;
+};
+
 export const Standard3P = addNeighbors(BaseMap);
 export const Standard4P = addNeighbors({
   ...BaseMap,
